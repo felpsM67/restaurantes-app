731eb8c4c9053aab16f4df045147531f8da39c1c
jhonata sobral <jhonata.sobral@ms.senac.br>
2025-07-24 21:04:50 -0400

    finalizando login

---
+++ src/componetes/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Button({
+  children,
+  onClick,
+  type = "button",
+  className = "",
+}: {
+  children: React.ReactNode | string;
+  onClick: () => void;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+}) {
+  return (
+    <button
+      type={type}
+      className={`w-full py-2 rounded ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
